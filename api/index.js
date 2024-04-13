44c6226560a304cd51a2e93cce1b3d7cc85c8269
jram828 <128335805+jram828@users.noreply.github.com>
2024-04-12 20:12:40 -0500

    DB

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 const Port = process.env.PORT || 3001;
 
-conn.sync({ alter: true }).then(() => {
+conn.sync({ force: true }).then(() => {
   server.listen(Port, () => {
     console.log("Server en puerto: " + Port); // eslint-disable-line no-console
   });
